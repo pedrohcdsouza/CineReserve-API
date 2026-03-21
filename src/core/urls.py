b38f032f8fdb291b1f0ef9834e1f69f8776134e8
= <=>
2026-03-20 22:13:14 -0300

    feat(Login): adding login case

--- src/core/urls.py
+++ src/core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -25,4 +25,5 @@ urlpatterns = [
     path(
         "api/doc/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
     ),
+    path("api/auth/", include("accounts.urls")),
 ]
